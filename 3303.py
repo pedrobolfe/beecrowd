b9b41da4373049104025c435179d0add7314125b
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-03-09 16:33:02 -0300

    Update 3303.py

--- 3303.py
+++ 3303.py
@@ -4,10 +4,3 @@ if len(palavra) >= 10:
     print('palavrao')
 else:
     print('palavrinha')
-
-# caracter= [list(i) for i in palavra]#cria uma lista com cada caracter da palavra
-
-# if len(caracter) >= 10:
-#     print('palavrao')
-# else:
-#     print('palavrinha')
