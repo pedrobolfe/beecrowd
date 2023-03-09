304b7eb747f2c8b0bddc0b7ae1f6e4cb1867987c
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-03-09 16:21:49 -0300

    Create 1000.java

---
+++ 1000.java
@@ -0,0 +1,8 @@
+public class 1000
+{// Início da classe Main
+    // método main inicia a execução dp app java
+	public static void main(String[] args){// início do método main
+	    // Código
+	    System.out.println("Hello World!"); 
+	}// fim do método main
+}// fim da classe PrimeiroProgramaJava
