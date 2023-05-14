b7edc5472ada656a93767130db4fc219a31e5511
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-05-14 17:59:04 -0300

    Update 1581.py

--- 1581.py
+++ 1581.py
@@ -1,11 +1,18 @@
 for _ in range(int(input())):
+    idiomas = list()
+    cont = 0
+    aux = ''
     for _ in range(int(input())):
-        idiomas=list()
         idioma =input()
         idiomas.append(idioma)
-    test=idiomas[0]
-    for i in idiomas:
-        if i != test:
-            test='ingles'
-            break
-    print(test)
+        aux = idioma
+
+    for i in range(len(idiomas)):
+        if aux != idiomas[i-1]:
+            cont -= 1    
+        else:
+            cont+=1
+    if cont == len(idiomas):
+      print(aux)    
+    else:
+      print("ingles")
