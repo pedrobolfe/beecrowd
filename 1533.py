e4c6dadab26f361d18ee372c3f8ebe0c1e4db03c
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-19 17:39:56 -0300

    add comentarios

--- 1533.py
+++ 1533.py
@@ -2,6 +2,6 @@ while True:
     num = input()
     if num == '0':
         break
-    indices = list(map(int, input().split()))
-    indices[indices.index(max(indices))] = 0 # o 1 maior numero  vira 0
+    indices = list(map(int, input().split())) # cria uma lista de inteiros dos inputs
+    indices[indices.index(max(indices))] = 0 # indices na pos do 1 maior numero vira 0
     print(indices.index(max(indices)) + 1) # printa o indice do 2 maior numero + 1
