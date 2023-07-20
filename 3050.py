c4980b81b2d735af5891196ee51dd0436e806f60
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-20 15:52:54 -0300

    Create 3050.py

---
+++ 3050.py
@@ -0,0 +1,13 @@
+#3050
+n = int(input())
+predios = list(map(int,input().split()))
+
+aux = 0
+
+for i in range(n): # pos da list
+    for j in range(n): 
+        cont = predios[i] + predios[j] + abs(i - j)
+        if cont > aux:
+            aux = cont
+        
+print(aux)
